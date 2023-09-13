5e73f902cba382975fa0923f2188a14027be0425
circuitbreak98 <circuitbreak98@gmail.com>
2023-09-13 15:42:40 +0900

    add expander and CSP examples - 시험 소요일자 expander UI 안으로 숨김 - CSP 예제 작성

--- src/app/main.py
+++ src/app/main.py
@@ -3,9 +3,9 @@ import datetime
 import networkx as nx
 import pandas as pd
 import streamlit as st
-
 from st_aggrid import AgGrid
 
+from csp import CSP, Constraint
 
 # Load the Excel data and clean it
 def load_data():
@@ -15,10 +15,13 @@ def load_data():
 
 
 # Create dependency edges from the dataframe
-def create_dependency_edges(dependency_list, configs):
+def create_dependency_edges_reserved(dependency_list, configs):
     dependencies = [[elem, lst[0], 1] for lst in dependency_list for elem in lst[1:] if not pd.isna(elem)]
     return dependencies
 
+def create_dependency_edges(dependency_list, configs):
+    dependencies = [[elem, lst[0], 1] for lst in dependency_list for elem in lst[1:] if not pd.isna(elem)]
+    return dependencies
 
 # Sort the documents based on their dependencies
 def sort_ordered_docs(documents, dependencies):
@@ -62,7 +65,7 @@ def app():
     st.sidebar.subheader("설정")
 
     df = load_data()
-    
+
     safety_analysis_included = st.sidebar.checkbox("안전성 분석 보고서 포함", value=True)
     cyber_security_included = st.sidebar.checkbox("사이버보안 평가 보고서 포함", value=True)
 
@@ -74,19 +77,20 @@ def app():
     
     filtered_df = df
 
-
     # Sample holidays list
     holidays = [pd.Timestamp("2023-01-01"), pd.Timestamp("2023-01-02")]
 
     # Assign publication dates conditions to a single configuration
     with st.sidebar.form('입력문서 발행일자 설정'):
-        spec_date = st.date_input("요구사항 명세서 발행일", value=datetime.date(2023,4,19))
-        design_date = st.date_input("설계 명세서 발행일", value=datetime.date(2023,4,26))
-        impl_date = st.date_input("구현 명세서 발행일", value=datetime.date(2023,5,10))
-        end_date = st.date_input("발행 마감일", value=datetime.date(2023,6,2))
-        ct_dates = st.number_input("CT 시험 수행일", min_value=1, value=3)
-        it_dates = st.number_input("IT 시험 수행일", min_value=1, value=3)
-        st_dates = st.number_input("ST 시험 수행일", min_value=1, value=3)
+        spec_date = st.date_input("요구사항 명세서 발행일",value=datetime.date.today())
+        design_date = st.date_input("설계 명세서 발행일", value=datetime.date.today()+datetime.timedelta(days=1))
+        impl_date = st.date_input("구현 명세서 발행일", value=datetime.date.today()+datetime.timedelta(days=2))
+        end_date = st.date_input("발행 마감일", value=datetime.date.today()+datetime.timedelta(days=3))
+        expander = st.expander("시험 수행 소요일자")
+        with expander:
+            ct_dates = st.number_input("CT 시험", min_value=1, value=3)
+            it_dates = st.number_input("IT 시험", min_value=1, value=3)
+            st_dates = st.number_input("ST 시험", min_value=1, value=3)
         submitted = st.form_submit_button('확인')
 
     if submitted:
@@ -94,8 +98,6 @@ def app():
 
     #TODO re-implement date scheduling algorithm
     ordered_docs, date_dependencies = select_publication_stages(filtered_df, None)
-    st.write("st.write(date_dependencies)")
-    st.write(date_dependencies)
     allocated_dates = {}
     
     #TODO re-implement date scheduling algorithm
